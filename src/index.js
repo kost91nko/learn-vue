8d1a2d88ccbec9c567432fc90bf7b29f386eec55
Konstantin Artemov <kost91nko@gmail.com>
2016-12-14 18:51:01 +0300

    Eslint changes and some learn tests

--- src/index.js
+++ src/index.js
@@ -1,10 +1,172 @@
 import './index.less';
+import _ from 'lodash';
+import axios from 'axios';
 
-let card = new Vue({
-  el: "#card",
-  data: {
-    title: "Cats",
-    content: `Cats are a diverse group of animals of the clade Dinosauria that first appeared 
-      during the Triassic period`
-  }
+window.testClick = new Vue({
+	el: '#click-test',
+	data: {
+		styleChild: {
+			border: '1px solid black',
+			height: '50px',
+			width: '100px',
+		},
+		styleParent: {
+			border: '1px solid red',
+		},
+	},
+	methods: {
+		childHandler: function (event) {
+			alert('Child text: ' + event.currentTarget);
+		},
+		parentHandler: function (event) {
+			alert('Parent text: ' + event.currentTarget);
+		},
+	}
+});
+
+window.card = new Vue({
+	el: '#card',
+	data: {
+		title: 'Cats',
+		content: `Cats are a diverse group of animals of the clade Dinosauria that first appeared
+      		during the Triassic period`,
+		items: [
+			{ id: 4, name: 'Wwwww' },
+			{ id: 1, name: 'First' },
+			{ id: 2, name: 'Second' }
+		]
+	}
+});
+
+window.card2 = new Vue({
+	el: '#card-2',
+	data: {
+		message: 'You loaded this page on ' + new Date()
+	}
+});
+
+window.app4 = new Vue({
+	el: '#app-4',
+	data: {
+		todos: [
+			{ text: 'Learn JavaScript' },
+			{ text: 'Learn Vue' },
+			{ text: 'Build something awesome' }
+		]
+	}
+});
+
+window.app5 = new Vue({
+	el: '#app-5',
+	data: {
+		message: 'Hello, Vue.js'
+	},
+	methods: {
+		reverseMessage: function () {
+			this.message = this.message.split('').reverse().join('');
+		}
+	}
+});
+
+
+window.app6 = new Vue({
+	el: '#app-6',
+	data: {
+		message: 'Hello Vue!'
+	}
+});
+
+window.app7 = new Vue({
+	el: '#example',
+	data: {
+		message: 'Hello'
+	},
+	computed: {
+		// a computed getter
+		reversedMessage: function () {
+			// `this` points to the vm instance
+			return this.message.split('').reverse().join('');
+		}
+	}
+});
+
+window.app7 = new Vue({
+	el: '#demo',
+	data: {
+		firstName: 'Foo',
+		lastName: 'Bar',
+		age: 10,
+		fullName: 'Foo Bar'
+	},
+	watch: {
+		firstName: function (val) {
+			this.fullName = val + ' ' + this.lastName;
+		},
+		lastName: function (val) {
+			this.fullName = this.firstName + ' ' + val;
+		}
+	},
+	computed: {
+		computedFullName: function () {
+			return this.firstName + ' ' + this.lastName + ' ' + this.age;
+		}
+	}
+});
+
+let Todo = {
+	template: '<span>PARAM PAMPARAM</span>'
+};
+// Since there is already a rich ecosystem of ajax libraries
+// and collections of general-purpose utility methods, Vue core
+// is able to remain small by not reinventing them. This also
+// gives you the freedom to just use what you're familiar with.
+window.watchExampleVM = new Vue({
+	el: '#watch-example',
+	data: {
+		question: '',
+		answer: 'I cannot give you an answer until you ask a question!',
+		currentTodo: Todo
+	},
+	watch: {
+		// whenever question changes, this function will run
+		question: function () {
+			this.answer = 'Waiting for you to stop typing...';
+			this.getAnswer();
+		}
+	},
+	methods: {
+		// _.debounce is a function provided by lodash to limit how
+		// often a particularly expensive operation can be run.
+		// In this case, we want to limit how often we access
+		// yesno.wtf/api, waiting until the user has completely
+		// finished typing before making the ajax request. To learn
+		// more about the _.debounce function (and its cousin
+		// _.throttle), visit: https://lodash.com/docs#debounce
+		getAnswer: _.debounce(
+			function () {
+				let vm = this;
+				if (this.question.indexOf('?') === -1) {
+					vm.answer = 'Questions usually contain a question mark. ;-)';
+					return;
+				}
+				vm.answer = 'Thinking...';
+				axios.get('https://yesno.wtf/api')
+					.then(function (response) {
+						vm.answer = _.capitalize(response.data.answer);
+					})
+					.catch(function (error) {
+						vm.answer = 'Error! Could not reach the API. ' + error;
+					});
+			},
+			// This is the number of milliseconds we wait for the
+			// user to stop typing.
+			500
+		)
+	},
+	components: {
+		'todo-item': {
+			template: '<span>IT\'S A NEW DAY IT\'s A NEW LIFE</span>'
+		},
+
+	}
 });
--- webpack.config.js
+++ webpack.config.js
@@ -3,117 +3,131 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    vendor: './src/vendor.js',
-    app: './src/index.js'
-  },
+	entry: {
+		vendor: './src/vendor.js',
+		app: './src/index.js'
+	},
 
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: '[name].js',
-    chunkFilename: '[id].[chunkhash].js'
-  },
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: '[name].js',
+		chunkFilename: '[id].[chunkhash].js'
+	},
 
-  resolve: {
-    modules: [
-      'node_modules',
-    ],
-    alias: {
-      'vue$': 'vue/dist/vue.common.js'
-    }
-  },
+	resolve: {
+		modules: [
+			'node_modules',
+		],
+		alias: {
+			'vue$': 'vue/dist/vue.common.js'
+		}
+	},
 
-  devtool: 'source-map',
+	devtool: 'source-map',
 
-  // What information should be printed to the console
-  stats: {
-    colors: true,
-    reasons: true,
-    hash: false,
-    version: true,
-    timings: true,
-    chunks: false,
-    chunkModules: false,
-    children: false,
-    cached: true,
-    cachedAssets: false,
-    modules: true,
-  },
+	// What information should be printed to the console
+	stats: {
+		colors: true,
+		reasons: true,
+		hash: false,
+		version: true,
+		timings: true,
+		chunks: false,
+		chunkModules: false,
+		children: false,
+		cached: true,
+		cachedAssets: false,
+		modules: true,
+	},
 
-  devServer: {
-    stats: {
-      assets: true,
-      colors: true,
-      reasons: false,
-      hash: false,
-      version: true,
-      timings: true,
-      chunks: true,
-      chunkModules: false,
-      chunkOrigins: false,
-      children: true,
-      cached: true,
-      cachedAssets: true,
-      errorDetails: true,
-      modules: false,
-      source: true
-    },
-    host: '0.0.0.0',
-    port: 8080,
-    open: false
-  },
+	devServer: {
+		stats: {
+			assets: true,
+			colors: true,
+			reasons: false,
+			hash: false,
+			version: true,
+			timings: true,
+			chunks: true,
+			chunkModules: false,
+			chunkOrigins: false,
+			children: true,
+			cached: true,
+			cachedAssets: true,
+			errorDetails: true,
+			modules: false,
+			source: true
+		},
+		host: '0.0.0.0',
+		port: 8080,
+		open: false
+	},
 
 
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: ['app', 'vendor']
-    }),
+	plugins: [
+		new webpack.optimize.CommonsChunkPlugin({
+			name: ['app', 'vendor']
+		}),
 
-    new webpack.ProvidePlugin({
-      'Vue': 'vue',
-    }),
+		new webpack.ProvidePlugin({
+			'Vue': 'vue',
+		}),
 
-    // Plugin for webpack 2
-    new webpack.LoaderOptionsPlugin({
-      debug: true,
-      minimize: false,
-      options: {
-        context: path.resolve(__dirname, '.'),
-        output: {
-          path: path.resolve(__dirname, './dist'),
-        },
-      },
-    }),
+		// Plugin for webpack 2
+		new webpack.LoaderOptionsPlugin({
+			debug: true,
+			minimize: false,
+			options: {
+				context: path.resolve(__dirname, '.'),
+				output: {
+					path: path.resolve(__dirname, './dist'),
+				},
+			},
+		}),
 
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    }),
-  ],
+		new HtmlWebpackPlugin({
+			template: './src/index.html'
+		}),
+	],
 
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        include: [
-          path.resolve(__dirname, './src')
-        ],
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.html$/,
-        loader: 'raw-loader'
-      },
-      {
-        test: /\.less$/,
-        loader: "style-loader!css-loader!less-loader"
-      },
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          // vue-loader options go here
-        }
-      }
-    ],
-  },
+	module: {
+		rules: [
+			{
+				enforce: 'pre',
+				test: /\.vue$/,
+				loader: 'eslint-loader',
+				include: path.resolve(__dirname, './src'),
+				exclude: /node_modules/
+			},
+			{
+				enforce: 'pre',
+				test: /\.js$/,
+				loader: 'eslint-loader',
+				include: path.resolve(__dirname, './src'),
+				exclude: /node_modules/
+			},
+			{
+				test: /\.jsx?$/,
+				include: [
+					path.resolve(__dirname, './src')
+				],
+				loader: 'babel-loader'
+			},
+			{
+				test: /\.html$/,
+				loader: 'raw-loader'
+			},
+			{
+				test: /\.less$/,
+				loader: "style-loader!css-loader!less-loader"
+			},
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader',
+				options: {
+					// vue-loader options go here
+				}
+			}
+		],
+	},
 };
